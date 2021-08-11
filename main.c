d0b43b40d7a4802f4d36a615385076423415fd59
k3m13 <66939530+K3M13@users.noreply.github.com>
2021-08-12 00:31:45 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-void DESIMALTOBINNER();//berfungsi untuk mendeklarasikan fungsi DESIMALTOBINNER
+void DESIMALTOBINNER();
 int main()
 {
 int nilai;
-printf("Masukkan nilai DESIMAL : ");//Perintah untuk user memasukkan nilai DESIMAL
-scanf("%d",&nilai);//Inputan nilai dari user yang di tampung dalam variable integer
-printf("\nNilai dari DESIMAL %d adalah : ",nilai);//Keluaran nilai yang di tampung dalam variable nilai integer 
-DESIMALTOBINNER(nilai);//Pemangilan fungsi DESIMALTOBINNER yang telah kita deklarasikan
+printf("Masukkan nilai DESIMAL : ");
+scanf("%d",&nilai);
+printf("\nNilai dari DESIMAL %d adalah : ",nilai); 
+DESIMALTOBINNER(nilai);
 }
 
-void DESIMALTOBINNER(int desimal)//Fungsi DESIMALTOBINNER yang bertipe Void yang tidak memiliki nilai jadi tidak memerlukan nilai balik / return 
+void DESIMALTOBINNER(int desimal)
 {
-   if (desimal>1)//Fungsi IF deperlukan untuk membandingkan dan melakukan perulangan sampai bernilai FALSE
+   if (desimal>1)
    {
-      DESIMALTOBINNER(desimal/2);//Fungsi if akan menjalankan perintah jika IF bernilai TRUE
+      DESIMALTOBINNER(desimal/2);
    }
-   printf("%d",desimal%2);//Fungsi if akan keluar dari perintah jika bernilai FALSE
+   printf("%d",desimal%2);
 }
